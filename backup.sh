a37c72dc2fc2b70bb6980268bc54a2e5378b5155
Caleb Short <crackthezer0@gamil.com>
2020-08-23 20:16:43 -0400

    Initial Commit

---
+++ backup.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+hostname=$(hostname)
+date=$(date +"%Y-%m-%d")
+backupDir="/backups"
+
+cd /
+echo $(pwd)
+# Create backup dir if it does not exist
+function backup ()
+{
+    if [ -d "$backupDir" ]; then
+        sudo tar -czvf "${backupDir}/${hostname}_${date}.tar.gz" "home/$(whoami)"
+        echo "Ran backup"
+    else
+        mkdir ${backupDir}
+        # Recursively call to create dir
+        echo "Backup dir created"
+        backup
+    fi
+}
+
+# Delete backups older than one month
+function deleteOldBackups()
+{
+    files=(${backupDir}/*)
+    for fileName in ${files[@]}; do
+        # IFS='_' read -ra DATE <<< "$fileName"
+        
+        # Check if date is older than one month
+        if [[ $fileName =~ [0-9]{4}-[0-9]{2}-[0-9]{2} ]]; then
+            echo "date regex: ${BASH_REMATCH[0]}"
+            if [ ! -z ${BASH_REMATCH[0]} ]; then
+                fileDate=$(date -d "${BASH_REMATCH[0]}")
+                cutoffDate=$(date -d "-1 month")
+
+                if [[ "$fileDate" > "$cutoffDate" ]]; then
+                    continue
+                else
+                    rm $fileName
+                    echo "Deleted ${fileName}"
+                    continue
+                fi
+            else
+                echo "Date is invalid"
+                continue
+            fi
+            echo "Filename is invalid"
+            continue
+        fi 
+    done
+}
+
+function main()
+{
+    deleteOldBackups
+    backup
+}
+main
